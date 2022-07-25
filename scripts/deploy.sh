93fee1bd720cacffa6cde3bccad8b3b9b7878596
Jiaqi Li <lijiaqigreat@gmail.com>
2022-07-25 22:24:10 +0000

    test github release.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+mkdir -p out
+
+# release
+cargo build --release
+cp target/release/base9-builder out/base9-builder
+
+# sample_mustache_data.min.json
+cargo run list-variables - > out/mustache_data.min.json
+
+# wasm
 wasm-pack build --target web
 wasm-opt -Oz pkg/base9_builder_bg.wasm
-cargo build --release
-
 scripts/monkey_patch_wasm.sh
